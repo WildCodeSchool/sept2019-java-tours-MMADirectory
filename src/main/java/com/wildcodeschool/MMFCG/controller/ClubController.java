ebaef44c0c1d0a6cdb89c772e9c21de2e5817e8d
Maxime Thonneau <thonneaum@gmail.com>
2020-01-13 15:20:19 +0100

    search by name done

--- src/main/java/com/wildcodeschool/MMFCG/controller/ClubController.java
+++ src/main/java/com/wildcodeschool/MMFCG/controller/ClubController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -41,5 +40,12 @@ public class ClubController {
 		public String getClubById(@RequestParam Long id, Model model) { 
 			model.addAttribute("fichesClub", repository.getOne(id));
 			return "fichesClub"; 
+	}
+
+	//Recherche de club par nom
+	@GetMapping("/search/club")
+	public String GetClubBySearch(Model model, @RequestParam String club){
+		model.addAttribute("clubs", repository.findByName(club));
+		return "liste_clubs_client";
 	}
 }
